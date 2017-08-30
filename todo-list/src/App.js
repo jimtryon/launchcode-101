6bd67049d13d1526a228a3d593528f29e188ed06
Jim Tryon <tryonjf@gmail.com>
2017-08-30 16:51:28 -0700

    Add todos list items

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -3,17 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [
+        'Learn React',
+        'Do Laundry'
+      ]
+    };
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to My TODO List</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <ul>
+          {this.state.todos.map((todo, index) => <li key={index}>{todo}</li>)}
+        </ul>
+      </div >
     );
   }
 }
